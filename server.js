a6b6b6b61bf562116d36e539eb34c79fba8ca19b
Sachin <sachin.rajput@forwardai.com>
2022-06-21 13:17:50 +0530

    initial commit
    - add the code with docker file

---
+++ server.js
@@ -0,0 +1,52 @@
+const { default: axios } = require("axios");
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT | 8080;
+const NAME = process.env.NAME;
+const OTHER_SERVER = process.env.OTHER_SERVER;
+
+app.use((req, res, next) => {
+    console.log(`${req.method} : ${req.path}`);
+    next();
+});
+
+app.get("/api/data", (req, res) => {
+    res.json({
+        message: `Server :: ${NAME}`,
+        status: "success",
+        data: "No Data Available from other Server",
+    });
+});
+
+const callOtherServer = async (url = OTHER_SERVER) => {
+    try {
+        if (url === undefined) {
+            return "URL is undefined";
+        }
+
+        const result = await axios.get(url);
+
+        return result.data;
+    } catch (error) {
+        console.log("Error: ", error);
+        return "Error Occurred";
+    }
+};
+
+app.get("/api/call", async (req, res) => {
+    const data = await callOtherServer();
+
+    console.log("Data from other Server :: ", data);
+
+    res.json({
+        message: `Server :: ${NAME}`,
+        status: "success",
+        data,
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server ::${NAME} Running on ${PORT}`);
+});
